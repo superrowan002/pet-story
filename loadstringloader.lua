fb8bb6a347054c9b582c6f9cdbe39fa404d53e91
superrowan002 <157720210+superrowan002@users.noreply.github.com>
2024-02-24 13:52:15 +0100

    Create loadstringloader.lua

---
+++ loadstringloader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 7071444234 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/script"))()
+elseif game.PlaceId == 5042581461 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/cruisestory%20script.lua"))()
+end
