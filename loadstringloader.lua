f89476b393d43943dc5ac6059a537f57d98411f8
superrowan002 <157720210+superrowan002@users.noreply.github.com>
2024-02-24 14:07:41 +0100

    Update loadstringloader.lua

--- loadstringloader.lua
+++ loadstringloader.lua
@@ -1,7 +1,11 @@
 if game.PlaceId == 7071444234 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/script"))()
-else
-  if game.PlaceId == 5042581461 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/cruisestory%20script.lua"))()
-  end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/script"))()
+  else
+    if game.PlaceId == 5042581461 then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/cruisestory%20script.lua"))()
+    else
+      if game.PlaceId == 5693152742 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/superrowan002/pet-story/master/airplane%204%20script.lua"))()
+      end
+    end
 end
